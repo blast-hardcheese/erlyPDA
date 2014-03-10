935687977d5dfbe20c1db191ec420b3e7106cc4e
Devon Stewart <blast@hardchee.se>
2014-03-09 19:25:04 -0700

    Moving rules out into rules.txt

--- rules.erl
+++ rules.erl
@@ -20,15 +20,6 @@ match(Current, Rules) ->
     lists:foldl(find_match(Current), {failure, Current}, Rules).
 
 % {Current, Transition}
-build_rules(_Path) ->
-    [
-        {{p0, e, z0},    {transition, p0, e}},
-        {{p0, "0", z0},  {transition, p1, "0", z0}},
-        {{p0, "0", "0"}, {transition, p1, "0", "0"}},
-        {{p1, "0", "0"}, {transition, p2, "0", "0"}},
-        {{p2, "0", "0"}, {transition, p3, "0", "0"}},
-        {{p3, "0", "0"}, {transition, p0, "0"}},
-        {{p0, "1", "0"}, {transition, p4, e}},
-        {{p4, "1", "0"}, {transition, p4, e}},
-        {{p4, e, z0},    {transition, p4, e}}
-    ].
+build_rules(Path) ->
+    {ok, Rules} = file:consult(Path),
+    Rules.
